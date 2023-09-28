d9d04c8df46766223ccd8412f683d8b5f39a358e
Adrian Dobrzański <dobrzanski.dev@gmail.com>
2023-09-28 21:18:39 +0200

    chore: shrink initial repo
    Remove initial placeholder code and migrate to standalone components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
 })
-export class AppComponent {
-  title = 'starships-and-people';
-}
+export class AppComponent {}
